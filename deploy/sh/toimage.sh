94830eed1cce61c276df4b964826df6d6fcd749a
郑麒麟 <zhengqilin@joyme.sg>
2022-10-03 12:30:50 +0800

    pipeline

--- deploy/sh/toimage.sh
+++ deploy/sh/toimage.sh
@@ -6,9 +6,13 @@ IMG_NAME="my/nginx"
 IMG_TAG="1.0.1"
 IMG_FULL_NAME="${IMG_REGISTRY}/${IMG_NAME}:${IMG_TAG}"
 
+echo "login registry!"
 docker login -u t1 -p 'Oa123!@#' 192.144.178.98:443
+echo "build image!"
 docker build -t ${IMG_FULL_NAME} -f ../../project/Dockerfile .
+echo "push image!"
 docker push ${IMG_FULL_NAME}
+echo "rmi image!"
 docekr rmi ${IMG_FULL_NAME}
 
 sed -i "s#{{IMG_NAME}}#${IMG_FULL_NAME}#g" ../deploy.yaml
