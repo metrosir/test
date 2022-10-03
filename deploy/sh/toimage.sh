8964baa8e3ceb5e05ef1a2608e62181f48b73bf6
郑麒麟 <zhengqilin@joyme.sg>
2022-10-03 12:26:05 +0800

    pipeline

--- deploy/sh/toimage.sh
+++ deploy/sh/toimage.sh
@@ -7,7 +7,7 @@ IMG_TAG="1.0.1"
 IMG_FULL_NAME="${IMG_REGISTRY}/${IMG_NAME}:${IMG_TAG}"
 
 docker login -u t1 -p 'Oa123!@#' 192.144.178.98:443
-docker build -t ${IMG_FULL_NAME} -f ../../project/Dockerfile
+docker build -t ${IMG_FULL_NAME} -f ../../project/Dockerfile .
 docker push ${IMG_FULL_NAME}
 docekr rmi ${IMG_FULL_NAME}
 
